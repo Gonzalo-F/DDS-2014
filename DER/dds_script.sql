ebb6e8933ffe27c1b5c3a62cc4a843ab132f77f7
Lucas Pugnali <pugnali.lucas@gmail.com>
2014-10-20 17:34:39 -0300

    Agrego Tabla Jugadores

--- DER/dds_script.sql
+++ DER/dds_script.sql
@@ -1,9 +1,10 @@
 -- CREACION DE SCHEMA
 -- Comprueba si no existe ninguno, sino existe lo crea.
+USE DDS2014
 
 IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'GRUPO_1')
 BEGIN
-	EXEC ('CREATE SCHEMA GRUPO_1 AUTHORIZATION gd')
+	EXEC ('CREATE SCHEMA GRUPO_1 AUTHORIZATION dbo')
 END
 
 -- FIN CREACION DE SCHEMA
@@ -18,12 +19,17 @@ DROP TABLE Jugador
 
 -- CREACION DE TABLAS
 
-CREATE TABLE GRUPO_1.Jugador
+CREATE TABLE GRUPO_1.Jugadores
 (
-	id numeric(18,0) IDENTITY(1,1),
-	username nvarchar(50),
-	PRIMARY KEY (id)
-	FOREIGN KEY (visibilidad_id) REFERENCES LOS_SUPER_AMIGOS.Visibilidad (id),
+Id_Jugador int identity(1,1) PRIMARY KEY,
+Nombre nvarchar(45),
+Apodo nvarchar (45),
+FechaNac date,
+Calle varchar(70),
+Handicap numeric(5,2),
+Promedio numeric(2,2)
 )
+
+
 
 -- FIN DE CREACION DE TABLAS
