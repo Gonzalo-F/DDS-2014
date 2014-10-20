eadb18b0a00c54139c4efa79eea6c8d4f81c252e
Gonzalo-F <gonzalo.frc@gmail.com>
2014-10-20 18:26:53 -0300

    Cambio el formato para declarar PK y FK

--- DER/dds_script.sql
+++ DER/dds_script.sql
@@ -14,10 +14,13 @@ END
 
 IF OBJECT_ID('GRUPO_1.Penalizaciones', 'U') IS NOT NULL
 DROP TABLE GRUPO_1.Penalizaciones
+
 IF OBJECT_ID('GRUPO_1.Calificaciones', 'U') IS NOT NULL
 DROP TABLE GRUPO_1.Calificaciones
+
 IF OBJECT_ID('GRUPO_1.Jugadores', 'U') IS NOT NULL
 DROP TABLE GRUPO_1.Jugadores
+
 IF OBJECT_ID('GRUPO_1.Partidos', 'U') IS NOT NULL
 DROP TABLE GRUPO_1.Partidos
 
@@ -28,40 +31,48 @@ DROP TABLE GRUPO_1.Partidos
 
 CREATE TABLE GRUPO_1.Jugadores
 (
-Id numeric(18,0) identity(1,1) PRIMARY KEY,
-Nombre nvarchar(45) NOT NULL,
-Apodo nvarchar (45) NOT NULL,
-FechaNac date NOT NULL,
-Handicap numeric(18,0) NOT NULL,
-Promedio numeric(18,2) NOT NULL
+	Id numeric(18,0) IDENTITY(1,1),
+	Nombre nvarchar(45) NOT NULL,
+	Apodo nvarchar (45) NOT NULL,
+	FechaNac date NOT NULL,
+	Handicap numeric(18,0) NOT NULL,
+	Promedio numeric(18,2) NOT NULL
+	PRIMARY KEY (id),
 )
 
 CREATE TABLE GRUPO_1.Partidos
 (
-Id numeric(18,0) identity(1,1) PRIMARY KEY,
-Lugar nvarchar(45) NOT NULL,
-Fecha date NOT NULL,
-Hora time NOT NULL,
-Abierto bit /* 1 Abierto, 0 Cerrado */ NOT NULL,
+	Id numeric(18,0) IDENTITY(1,1),
+	Lugar nvarchar(45) NOT NULL,
+	Fecha date NOT NULL,
+	Hora time NOT NULL,
+	Abierto bit /* 1 Abierto, 0 Cerrado */ NOT NULL,
+	PRIMARY KEY (id),
 )
 
 CREATE TABLE GRUPO_1.Calificaciones
 (
-Id numeric(18,0) identity(1,1) PRIMARY KEY,
-JugadorCalificado numeric(18,0) FOREIGN KEY REFERENCES GRUPO_1.Jugadores NOT NULL,
-JugadorCalificante numeric(18,0) FOREIGN KEY REFERENCES GRUPO_1.Jugadores NOT NULL,
-Descripcion varchar(45),
-Partido_Id numeric(18,0) FOREIGN KEY REFERENCES GRUPO_1.Jugadores NOT NULL
+	Id numeric(18,0) IDENTITY(1,1),
+	JugadorCalificado numeric(18,0) NOT NULL,
+	JugadorCalificante numeric(18,0) NOT NULL,
+	Descripcion varchar(45),
+	Partido_Id numeric(18,0) NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (JugadorCalificado) REFERENCES GRUPO_1.Jugadores (id),
+	FOREIGN KEY (JugadorCalificante) REFERENCES GRUPO_1.Jugadores (id),
+	FOREIGN KEY (Partido_Id) REFERENCES GRUPO_1.Partidos (id),
 )
 
 CREATE TABLE GRUPO_1.Penalizaciones
 (
-Id numeric(18,0) identity(1,1) PRIMARY KEY,
-Fecha date NOT NULL,
-Motivo varchar(45),
-Partido_Id numeric(18,0) FOREIGN KEY REFERENCES GRUPO_1.Partidos NOT NULL,
-Jugador_Id numeric(18,0) FOREIGN KEY REFERENCES GRUPO_1.Jugadores NOT NULL
+	Id numeric(18,0) IDENTITY(1,1),
+	Fecha date NOT NULL,
+	Motivo varchar(45),
+	Partido_Id numeric(18,0) NOT NULL,
+	Jugador_Id numeric(18,0) NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (Partido_Id) REFERENCES GRUPO_1.Partidos (id),
+	FOREIGN KEY (Jugador_Id) REFERENCES GRUPO_1.Jugadores (id),
 )
-
 
 -- FIN DE CREACION DE TABLAS
