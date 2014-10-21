0ff9c8f82a149c0ec88ce2b744b676a299c190d6
Lucas Pugnali <pugnali.lucas@gmail.com>
2014-10-21 13:05:44 -0300

    Ideando el punto 2

--- DER/soluciones_requerimientos.sql
+++ DER/soluciones_requerimientos.sql
@@ -9,11 +9,18 @@ select * from GRUPO_1.Amigos
 -- SOLUCIONES	REQUERIMIENTOS
 -- 1
 SELECT * FROM GRUPO_1.Jugadores_malos
+
 -- 2
+SELECT j.Id Id_Jugador, COUNT(*) Penalizaciones
+FROM GRUPO_1.Jugadores j,GRUPO_1.Penalizaciones p
+where j.Id=Jugador_Id
+GROUP BY j.Id
 
 -- 3
 SELECT * FROM GRUPO_1.Jugadores_malos WHERE YEAR(GETDATE()) - YEAR(fechaNac) < 25
+
 -- 4
 EXEC GRUPO_1.baja_con_reemplazo , , , , , 
+
 -- 5
 EXEC GRUPO_1.baja_sin_reemplazo ,
